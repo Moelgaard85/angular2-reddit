aefa9fbfdefe6f970901836d10adf42ac24bb0a8
Bjarne <bgp@proreact.dk>
2017-03-21 15:48:49 +0100

    Articles can be added and automatically sorted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { ArticleComponent } from './article/article.component';
 import { Article } from './article/article.model';
 import { Component } from '@angular/core';
 
@@ -11,15 +12,21 @@ export class AppComponent {
 
   constructor() {
     this.articles = [
-      new Article('Angular 2', 'http://test.dk', 3),
-      new Article('Fullstack', 'http://test.dk', 30),
-      new Article('Angular Homepage', 'http://test.dk', 14),
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.com', 30),
+      new Article('Angular Homepage', 'http://angularjs.com', 14),
     ];
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
 }
